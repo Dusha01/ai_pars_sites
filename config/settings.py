3ba50f3cb37a24f13b9db2e4db18a243a667bf64
Dusha_01 <87571857+Dusha01@users.noreply.github.com>
2025-04-16 00:39:51 +1000

    +

--- config/settings.py
+++ config/settings.py
@@ -4,12 +4,10 @@ import os
 load_dotenv()
 
 class Setting:
-    #LLM_MODEL = "deepseek-chat-v3-0324"
     LLM_MODEL = "gpt-4o-mini"
     MAX_TOKENS = 1000
     CACHE_SIZE = 5
-    BASE_URL = "https://llm.drakesoft.ru/api/v1"
+    BASE_URL = "https://api.aitunnel.ru/v1/"
 
     class API:
-        DEEPSEEK = os.getenv("DEEPSEEK_API_KEY") 
-    
+        AI = os.getenv("API_KEY")
